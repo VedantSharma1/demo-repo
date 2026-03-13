88335242b6fee0071d915f48abc6ceea7f08f5ac
VedantSharma1 <58086983+VedantSharma1@users.noreply.github.com>
2026-03-13 23:12:10 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require("express")
+const app = express()
+
+app.get("/sum", function(req, res){
+  const a = parseInt(req.query.a)
+  const b = parseInt(req.query.b)
+
+  res.json({
+    ans: a + b
+  })
+})
+
+app.get("/sub", function(req, res){
+  
+})
+
+app.get("multiply", function(req, res){
+  
+})
+
+app.listen(3000)
